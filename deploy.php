7d06b7eb6cd4208bc05a17eeaa10f57ca9f69116
Bruno Dilhof <brunodilhof@gmail.com>
2025-05-26 16:16:19 +0200

    Deployer config

---
+++ deploy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Deployer;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require 'recipe/laravel.php';
+require 'recipe/deploy/push.php';
+
+$dotenv = new Dotenv;
+$dotenv->load(__DIR__ . '/.env');
+
+// Global Config
+set('repository', 'git@github.com:dp-bratislava/app-wtf.git');
+set('php_fpm_version', '8.2');
+set('keep_releases', 3);
+set('nvm', 'source $HOME/.nvm/nvm.sh &&');
+set('composer_options', '--ignore-platform-reqs');
+set('bin/php', '/usr/bin/php8.2');
+set('bin/composer', '/usr/bin/php8.2 /usr/local/bin/composer');
+
+// Hosts
+host('staging')
+    ->setHostname(env('DEPLOYER_STAGING_HOSTNAME'))
+    ->setRemoteUser(env('DEPLOYER_STAGING_REMOTE_USER'))
+    ->setDeployPath(env('DEPLOYER_STAGING_DEPLOY_PATH'))
+    ->set('branch', env('DEPLOYER_STAGING_BRANCH'))
+    ->set('use_nvm', true);
+
+/*
+host('production')
+    ->setHostname(env('DEP_PROD_HOSTNAME'))
+    ->setRemoteUser(env('DEP_PROD_REMOTE_USER'))
+    ->setDeployPath(env('DEP_PROD_DEPLOY_PATH'))
+    ->set('branch', env('DEP_PROD_BRANCH'))
+    ->set('use_nvm', false);
+*/
+
+// Tasks
+task('build', function () {
+    $useNvm = get('use_nvm');
+    if ($useNvm) {
+        run('cd {{release_path}} && {{nvm}} npm install && {{nvm}} npm run build');
+    } else {
+        run('cd {{release_path}} && npm install && npm run build');
+    }
+});
+
+// Hooks
+after('deploy:failed', 'deploy:unlock');
+after('deploy:symlink', 'build');
+after('deploy', 'artisan:cache:clear');
