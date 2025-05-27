ca168ea49c4a17bbb8b032fcf1da4c630ecfd4bf
Bruno Dilhof <brunodilhof@gmail.com>
2025-05-27 09:20:55 +0200

    Add WtfPlugin to admin panel seetup

--- config/admin-panel.php
+++ config/admin-panel.php
@@ -1,8 +1,11 @@
 <?php
 
+use Dpb\WorkTimeFundFilament\Filament\Plugins\WtffPlugin;
+
 return [
     'plugins' => [
         \Dpb\UserAdmin\UserAdminPlugin::class,
         \Dpb\DatahubSync\DatahubSyncPlugin::class,
+        WtffPlugin::class,
     ],
 ];
