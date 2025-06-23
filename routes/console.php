1496a8ee28be47ffcc716fa819ac1050220ad6f2
Bruno Dilhof <brunodilhof@gmail.com>
2025-06-23 11:49:56 +0200

    Run datahub-sync:update every hour

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('datahub-sync:update', [
+    '--models' => implode('', [
+        'Hierarchy',
+        'ContractType',
+        'Department',
+        'EmployeeCircuit',
+        'Employee',
+        'Profession',
+        'EmployeeContract',
+    ]),
+])->hourly();
