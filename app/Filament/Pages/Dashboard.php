eaa45d517f731dbfc8462153168d5fa29f399756
Andej Horny <horny.andrej@dpb.sk>
2025-10-22 09:47:06 +0200

    WIP adding dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
+
+class Dashboard extends BaseDashboard
+{
+    use HasFiltersForm;
+
+    protected static string $routePath = 'finance';
+    protected static ?string $title = 'Finance dashboard';
+
+    public function filtersForm(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        DatePicker::make('startDate'),
+                        DatePicker::make('endDate'),
+                        // ...
+                    ])
+                    ->columns(3),
+            ]);
+    }
+}
