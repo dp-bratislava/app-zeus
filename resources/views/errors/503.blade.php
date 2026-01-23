fc37a3caa1be0a976a071f229ebe3dcd178495fb
Bruno Dilhof <brunodilhof@gmail.com>
2026-01-23 13:20:50 +0100

    Add maintenance notice when using artisan down

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,4 @@
+<h1>Prebieha plánovaná údržba aplikácie Zeus</h1>
+<p>Aplikácia je momentálne dočasne nedostupná z dôvodu údržby.</p>
+<p>Prosíme o trpezlivosť a opakovanie pokusu neskôr.</p>
+<p>Odbor IT, klapka 1234</p>
