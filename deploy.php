40d21b5430211a9851c9ddeb100a451c77551e40
Bruno Dilhof <brunodilhof@gmail.com>
2026-02-04 17:26:30 +0100

    Deploy config

--- deploy.php
+++ deploy.php
@@ -62,15 +62,3 @@ task('artisan:view:cache', function () {
 after('deploy:failed', 'deploy:unlock');
 after('deploy:symlink', 'build');
 after('deploy', 'artisan:cache:clear');
-
-task('deploy', [
-    'deploy:prepare',
-    'deploy:vendors',
-    'artisan:storage:link',
-    'artisan:config:cache',
-    'artisan:route:cache',
-    'artisan:view:cache',
-    'artisan:event:cache',
-    'artisan:migrate',
-    'deploy:publish',
-]);
