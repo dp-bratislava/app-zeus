de3c1d1a7854de167ebe38a87e0ae171b576cf80
Andej Horny <horny.andrej@dpb.sk>
2026-02-10 10:12:55 +0100

    updated tests

--- tests/Feature/Filament/UIAccessTest.php
+++ tests/Feature/Filament/UIAccessTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature\Filament;
 use Tests\TestCase;
 use App\Models\User;
 use Dpb\DpbEmployeeManager\Filament\Pages\EmployeeManagerPage\EmployeeManagerPage;
+use Dpb\Package\TaskMS\UI\Filament\Resources\Inspection\InspectionAssignmentResource;
+use Dpb\Package\TaskMS\UI\Filament\Resources\Inspection\InspectionTemplateResource;
 use Dpb\Package\TaskMS\UI\Filament\Resources\Task\PlaceOfOriginResource;
 use Dpb\Package\TaskMS\UI\Filament\Resources\Task\TaskItemGroupResource;
 use Dpb\Package\TaskMS\UI\Filament\Resources\Task\TaskItemResource;
@@ -20,7 +22,7 @@ class UIAccessTest extends TestCase
     // use RefreshDatabase; // <-- handles migrations automatically
     use FilamentPermissionAssertions;
 
-    public function test_predavacky_can_access()
+    public function test_predavacky_veduci_can_access()
     {
         $user = User::where('login', '=', 'predavacky')->first();
 
@@ -38,15 +40,19 @@ class UIAccessTest extends TestCase
         }
     }
 
-    public function test_predavacky_can_not_access()
+    public function test_predavacky_veduci_can_not_access()
     {
         $user = User::where('login', '=', 'predavacky')->first();
         $resources = [
+            // task
             PlaceOfOriginResource::class,
             TaskItemGroupResource::class,
             TaskItemResource::class,
-            DailyMaintenanceResource::class,
-            TaskAssignmentResource::class,
+            // DailyMaintenanceResource::class,
+            // TaskAssignmentResource::class,
+            // inspection
+            InspectionAssignmentResource::class,
+            InspectionTemplateResource::class
         ];
 
         foreach ($resources as $resource) {
@@ -57,6 +63,40 @@ class UIAccessTest extends TestCase
         // $this->assertUserCannotAccessResource($user, TaskItemResource::class);
         // $this->assertUserCannotAccessResource($user, DailyMaintenanceResource::class);
         // $this->assertUserCannotAccessResource($user, TaskAssignmentResource::class);
+    }
+
+    public function test_veduci_pracovnik_can_access()
+    {
+        $user = User::where('login', '=', 'T0001')->first();
+
+        $pages = [
+            // WorktimeSchedulePlannerPage::class,
+            WorktimeManagementPage::class,
+            EmployeeManagerPage::class,
+        ];
+
+        foreach ($pages as $page) {
+            $this->assertTrue(
+                $this->assertUserCanAccessPage($user, $page),
+                "User '{$user->login}' should have access to {$page}"
+            );
+        }
+
+        $resources = [
+            // task
+            PlaceOfOriginResource::class,
+            TaskItemGroupResource::class,
+            TaskItemResource::class,
+            // DailyMaintenanceResource::class,
+            // TaskAssignmentResource::class,
+            // inspection
+            InspectionAssignmentResource::class,
+            InspectionTemplateResource::class
+        ];       
+        
+        foreach ($resources as $resource) {
+            $this->assertUserCanAccessResource($user, $resource);
+        }             
     }
 
     public function test_superadmin_permissions()
