51501359abecf49e2ee3954cb10306154ae92ab9
iaol12 <frantisekboldis@gmail.com>
2026-03-04 14:56:58 +0100

    change worktime all departments sync schedule to start daily at 00:10 am

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('datahub:update')->hourly();
 
-Schedule::command('dpb-work-time-fund:sync-worktimes-for-all-departments')->everyTenMinutes();
+Schedule::command('dpb-work-time-fund:sync-worktimes-for-all-departments')->dailyAt('00:10');
