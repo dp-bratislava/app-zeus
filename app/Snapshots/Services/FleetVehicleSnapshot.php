b96765f1c08711ed60efcfc8520ac866d951fb9a
Andej Horny <horny.andrej@dpb.sk>
2026-05-05 14:52:00 +0200

    added fleet vehicle snapshot

--- app/Snapshots/Services/FleetVehicleSnapshot.php
+++ app/Snapshots/Services/FleetVehicleSnapshot.php
@@ -48,11 +48,50 @@ class FleetVehicleSnapshot implements SnapshotContract
         );
     }
 
+    public function vehicleLatestCodeCTE(string $uri = 'latest_code'): string
+    {
+        return "
+            {$uri} AS (
+                SELECT vehicle_id, vehicle_code_id
+                FROM (
+                    SELECT *,
+                        ROW_NUMBER() OVER (PARTITION BY vehicle_id ORDER BY date_from DESC) rn
+                    FROM fleet_vehicle_code_history
+                ) x
+                WHERE rn = 1
+            )
+        ";
+    }
+
+    public function vehicleLatestPlateCTE(string $uri = 'latest_plate'): string
+    {
+        return "
+            {$uri} AS (
+                SELECT vehicle_id, licence_plate_id
+                FROM (
+                    SELECT *,
+                        ROW_NUMBER() OVER (PARTITION BY vehicle_id ORDER BY date_from DESC) rn
+                    FROM fleet_licence_plate_history
+                ) x
+                WHERE rn = 1
+            )
+        ";
+    }
+
+    protected function with(string $tempTable): string
+    {
+        return "
+            {$this->vehicleLatestCodeCTE()},
+            {$this->vehicleLatestPlateCTE()}
+        ";
+    }
+
     public function buildInsertSql(string $tempTable): string
     {
         return "
             INSERT INTO {$this->targetTable()}
             ({$this->columns()})
+            WITH {$this->with($tempTable)}   
             SELECT
                 {$this->select()}
             {$this->from()}
@@ -66,8 +105,8 @@ class FleetVehicleSnapshot implements SnapshotContract
     {
         return [
             'vehicle_id' => 'v.id',
-            'code' => 'NULL',
-            'licence_plate' => 'NULL',
+            'code' => 'vc.`code`',
+            'licence_plate' => 'lp.`code`',
             'model' => 'vm.title',
             'type' => 'vt.title',
             'updated_at' => 'v.updated_at',
@@ -90,6 +129,10 @@ class FleetVehicleSnapshot implements SnapshotContract
             JOIN {$tempTable} tmp ON tmp.id = v.id
             LEFT JOIN fleet_vehicle_models vm ON vm.id = v.model_id
             LEFT JOIN fleet_vehicle_types vt ON vt.id = vm.type_id
+            LEFT JOIN latest_code lc ON lc.vehicle_id = v.id
+            LEFT JOIN fleet_vehicle_codes vc ON vc.id = lc.vehicle_code_id
+            LEFT JOIN latest_plate lp_ref ON lp_ref.vehicle_id = v.id
+            LEFT JOIN fleet_licence_plates lp ON lp.id = lp_ref.licence_plate_id
         ";
     }
 
