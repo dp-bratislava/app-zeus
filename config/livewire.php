19760c84c061af261dee2678ec16532a452900ac
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-06-11 08:28:38 +0200

    add livewire config

---
+++ config/livewire.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'payload' => [
+        'max_size' => 1048576 * 10,
+        'max_nesting_depth' => 10,
+        'max_calls' => 50,
+        'max_components' => 20
+    ]
+];
