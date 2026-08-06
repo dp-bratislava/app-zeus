309a201958433aed931b35985be175471e881bab
iaol12 <frantisekboldis@gmail.com>
2026-08-06 11:40:41 +0200

    refactor dashboard to extend BaseDashboard and dynamically generate links

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Page;
+use Filament\Resources\Resource;
 
-class Dashboard extends Page
+class Dashboard extends BaseDashboard
 {
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-document-text';
 
@@ -12,6 +14,42 @@ class Dashboard extends Page
 
     public static function shouldRegisterNavigation(): bool
     {
-        return false; // We override the default dashboard, so no sidebar item
+        return false;
+    }
+
+    public function getLinks(): array
+    {
+        return collect(config('admin-panel.navigation.items', []))
+            ->filter(fn (string $class) => $this->canAccessItem($class))
+            ->map(fn (string $class) => [
+                'label' => $class::getNavigationLabel(),
+                'icon' => $class::getNavigationIcon(),
+                'url' => $this->urlFor($class),
+            ])
+            ->values()
+            ->all();
+    }
+
+    private function canAccessItem(string $class): bool
+    {
+        if (is_subclass_of($class, Resource::class)) {
+            return $class::canViewAny();
+        }
+
+        if (is_subclass_of($class, Page::class)) {
+            return $class::canAccess();
+        }
+
+        return true;
+    }
+
+    private function urlFor(string $class): string
+    {
+        if (is_subclass_of($class, Resource::class)) {
+            return $class::getUrl('index');
+        }
+
+        return $class::getUrl();
     }
 }
+
